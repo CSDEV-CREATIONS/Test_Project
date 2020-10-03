c58bc21b1532c62a5f4f7ad4fc5b46138ec2c5a8
Chris Stockdale <cjsgamingnetwork92@gmail.com>
2020-10-03 04:22:32 -0500

    C# Code Commits

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Test_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Chris";
+
+            int age = 28;
+
+            Console.WriteLine("Hello my name is " + name + " and I am " + age + " years old");
+        }
+    }
+}
